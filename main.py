991e406163b7ebc41572f97050a2c99b08a5d35b
hacker64-pixel <59923822+hacker64-pixel@users.noreply.github.com>
2020-03-07 18:14:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("opis tijela 1.0")
+kosa = input("koja je boja kose:")
+koza = input("koja je boja koze:")
+majica = input("kakva je majca:")
+hlaće = input("kakve su hlace:")
+patike = input("kakve su patike:")
+print("boja kose je "+ kosa +" a koza je "+ koza +". majca mu je "+ majica +" hlaće su mu "+ hlaće +". a patike su mu "+ patike
+import time
+time.sleep(19)
+import supprot
---
+++ supprot.py
@@ -0,0 +1,6 @@
+import webbrowser
+a_website = "https://github.com/hacker64-pixel"
+webbrowser.open_new(a_website)
+print("follow me")
+import time
+time.sleep(38)
